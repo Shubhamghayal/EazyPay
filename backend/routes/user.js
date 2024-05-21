ed37afd249097a87627c9b54a43990fa55bc37d6
Shubham <shubhamghayal009@gmail.com>
2024-05-22 01:56:14 +0530

    update user.js,appbar,button,signin

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -91,5 +91,29 @@ router.post("/signin", async (req, res) => {
         message: "Error while logging in"
     })
 })
+router.get("/bulk", async (req, res) => {
+    const filter = req.query.filter || "";
+
+    const users = await User.find({
+        $or: [{
+            firstName: {
+                "$regex": filter
+            }
+        }, {
+            lastName: {
+                "$regex": filter
+            }
+        }]
+    })
+
+    res.json({
+        user: users.map(user => ({
+            username: user.username,
+            firstName: user.firstName,
+            lastName: user.lastName,
+            _id: user._id
+        }))
+    })
+})
 
 module.exports = router;
